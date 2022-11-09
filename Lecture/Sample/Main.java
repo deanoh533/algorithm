7663c262bfffa8e66e38626cbdbd74029e6153aa
deanoh533 <deanoh533@gmail.com>
2022-11-09 21:04:35 +0900

    04.단어뒤집기

---
+++ Lecture/Sample/Main.java
@@ -0,0 +1,22 @@
+package Lecture.Sample;
+
+import java.util.Scanner;
+
+public class Main {
+    public String solution(String str){
+
+        String answer = "";
+
+        
+        return answer;
+
+    }
+    
+    public static void main(String[] args) {
+        Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.println(T.solution(str));
+
+    }
+}
