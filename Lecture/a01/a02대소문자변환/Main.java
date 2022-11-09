e1fafcf181ae5f4a9654217a8597ebdfc1878837
deanoh533 <deanoh533@gmail.com>
2022-11-09 18:45:19 +0900

    03.문장속단어

--- Lecture/a01/a02대소문자변환/Main.java
+++ Lecture/a01/a02대소문자변환/Main.java
@@ -8,16 +8,18 @@ public class Main {
 
         String answer = "";
 
+        // for (char x : str.toCharArray()){
+        //     if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
+        //     else answer+=Character.toLowerCase(x);
+        // } 
+
         for (char x : str.toCharArray()){
-            if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
-            else answer+=Character.toLowerCase(x);
+          if(x>=97 && x<=122) answer+=(char)(x-32);
+          else answer+=(char)(x+32);
         }
 
         return answer;
     }
-
-
-
 
   public static void main(String[] args){
     Main T = new Main();
